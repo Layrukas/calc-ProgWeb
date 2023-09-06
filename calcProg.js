2eeebb96ab8a1ce7082c91efee7b74c2bc31b805
Layrukas <103163736+Layrukas@users.noreply.github.com>
2023-09-06 17:42:26 -0300

    Add files via upload

---
+++ calcProg.js
@@ -0,0 +1,32 @@
+var numero;
+var numero2;
+
+function soma(){
+
+    num1 =document.getElementById("num1").value;
+    num2 =document.getElementById("num2").value;
+    let resultado = Number(num1) + Number(num2);
+    document.getElementById("resultado").innerHTML = resultado;
+
+}
+
+function subtracao(){
+    num1 =document.getElementById("num1").value;
+    num2 =document.getElementById("num2").value;
+    let resultado =Number(num1) - Number(num2);
+    document.getElementById("resultado").innerHTML = resultado;
+}
+
+function divisao(){
+    num1 =document.getElementById("num1").value;
+    num2 =document.getElementById("num2").value;
+    let resultado = Number(num1) / Number(num2);
+    document.getElementById("resultado").innerHTML = resultado;
+}
+
+function multiplicacao(){
+    num1 =document.getElementById("num1").value;
+    num2 =document.getElementById("num2").value;
+    let resultado =Number(num1) * Number(num2);
+    document.getElementById("resultado").innerHTML = resultado;
+}
