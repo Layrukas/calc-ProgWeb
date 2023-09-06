a38f091dd9074704ac42a8b6ee2c847c2bbfb059
Layrukas <103163736+Layrukas@users.noreply.github.com>
2023-09-06 16:52:39 -0300

    Add files via upload

---
+++ calcProg.js
@@ -0,0 +1,20 @@
+var numero;
+var numero2;
+
+function soma(numero, numero2){
+    return (numero + numero2);
+}
+
+function subtracao(numero, numero2){
+    return (numero - numero2);
+}
+
+function divisao(numero, numero2){
+    if(numero == 0 || numero2 == 0){
+        return ("nao pode dividir por");
+    }else{return (numero / numero2);}
+}
+
+function multiplicacao(numero, numero2){
+    return (numero * numero2);
+}
